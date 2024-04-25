4b9867bdae5c5bcd72d64db46d7c9d64a43cd2d5
colonsemi <colonsemi.solution@gmail.com>
2024-04-25 23:05:23 +0530

    remove slider

--- src/modules/Home/component/Carousal.jsx
+++ src/modules/Home/component/Carousal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+// import OwlCarousel from 'react-owl-carousel';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const Home = () => {
     return (
